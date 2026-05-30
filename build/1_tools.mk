c7c49f2c9898d2ddf2acf88c7cdc499ba9864bff
dulstack <doowew100@gmail.com>
2026-05-30 16:19:48 +0200

    Added configuration flags for tools 	Solved chaotic build sequence

---
+++ build/1_tools.mk
@@ -0,0 +1,14 @@
+.PHONY: tools download-tools tools-build 
+
+TOOL_SUBMAKEFILES     = $(wildcard $(PWD)/build/tools/*.mk)
+TOOL_PACKAGES         = $(TOOL_SUBMAKEFILES:$(PWD)/build/tools/%.mk=%)
+TOOL_DOWNLOAD_TARGETS = $(TOOL_PACKAGES:%=download-%)
+TOOLS_ROOT            = $(PWD)/$(OUT)/tools
+TOOLS_CFLAGS          = -O2
+
+include $(TOOL_SUBMAKEFILES)
+
+tools: download-tools tools-build
+
+download-tools: $(TOOL_DOWNLOAD_TARGETS)
+tools-build: $(TOOL_PACKAGES)
