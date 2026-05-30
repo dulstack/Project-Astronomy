f1a79667e7a8e6142a03177e8714fc4f7084ee33
dulstack <doowew100@gmail.com>
2026-05-30 19:07:26 +0200

    Made sample package simpler to use 	Fixed some mistakes

--- build/.package_sample.mk
+++ build/.package_sample.mk
@@ -1,21 +1,24 @@
 # Sample package. Can be copied with a different name to add a new package.
 # After copying, replace everything here from 'package' to another package name.
-# Modify the variables below the line with .PHONY. Targets "package" and 
-# "download-package" should match the file name. For example:
-# file "file.mk" will have targets named "file" and "download-file", other can
-# have any name.
- 
-.PHONY: package download-package package-untar package-configure package-build package-install
+# Modify the variables below this line. Targets "package" and
+# "download-package" should match the file name of package, so make sure the
+# variable FILENAME is set correctly
 
+
+package_FILENAME     = ".package_sample.mk"
 package_VERSION      = 1.0
 package_TARBALL      = package-$(package_VERSION).tar.gz
 package_URL          = https://example.com/$(package_TARBALL)
 package_DIR          = package-$(package_VERSION)
-package_CONFIG_FLAGS = --prefix=$(PWD)/$(ROOTFS) CFLAGS="$(CFLAGS)"
+package_CONFIG_FLAGS = --prefix=/$(PWD)/$(ROOTFS) CFLAGS="$(CFLAGS)"
 
-package: download-package package-untar package-configure package-build package-install
+#Alias for target
+.PHONY: package $(package_FILENAME) download-$(package_FILENAME) package-untar package-configure package-build package-install
+package: $(package_FILENAME)
 
-download-package: $(OUT)/.downloaded_package_stamp
+$(package_FILENAME): download-$(package_FILENAME) package-untar package-configure package-build package-install
+
+download-$(package_FILENAME): $(OUT)/.downloaded_package_stamp
 package-untar: $(OUT)/.unpacked_package_stamp
 package-configure: $(OUT)/.configured_package_stamp
 package-build: $(OUT)/.built_package_stamp
--- build/1_tools.mk
+++ build/1_tools.mk
@@ -3,12 +3,15 @@
 TOOL_SUBMAKEFILES     = $(wildcard $(PWD)/build/tools/*.mk)
 TOOL_PACKAGES         = $(TOOL_SUBMAKEFILES:$(PWD)/build/tools/%.mk=%)
 TOOL_DOWNLOAD_TARGETS = $(TOOL_PACKAGES:%=download-%)
-TOOLS_ROOT            = $(PWD)/$(OUT)/tools
+TOOLS_ROOT            = $(OUT)/tools
 TOOLS_CFLAGS          = -O2
 
 include $(TOOL_SUBMAKEFILES)
 
-1_tools: download-1_tools tools-build
+1_tools: download-1_tools tools-build | $(TOOLS_ROOT)
 
 download-1_tools: $(TOOL_DOWNLOAD_TARGETS)
 tools-build: $(TOOL_PACKAGES)
+
+$(TOOLS_ROOT):
+	mkdir -p $@
